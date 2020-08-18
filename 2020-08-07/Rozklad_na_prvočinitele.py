f307322172bbcd3e5715518e8b40ba615728b207
Dhoozy <maximpolak@email.cz>
2020-08-18 11:41:30 +0200

    Převod na funkci Pozor, zjišťuje dělitele, ale ne prvočinitele!!!

--- 2020-08-07/Rozklad_na_prvočinitele.py
+++ 2020-08-07/Rozklad_na_prvočinitele.py
@@ -1,5 +1,13 @@
 cislo = int(input("Zadejte číslo, které chcete rozložit na prvočinitele: "))
 
-for prvocinitel in range(1, cislo + 1):
-    if cislo % prvocinitel == 0:
-        print(prvocinitel)
+def rozloz_na_prvocinitele(cislo):
+    seznam_prvocinitelu = []
+    for prvocinitel in range(1, cislo + 1):
+        if cislo % prvocinitel == 0:
+            seznam_prvocinitelu.append(prvocinitel)
+    return seznam_prvocinitelu
+
+seznam = rozloz_na_prvocinitele(cislo)
+
+for prvek in seznam:
+    print(prvek)
