cd86432c8d0ef25a6242104dbfb3cb5eb2ecd9c4
Dhoozy <maximpolak@email.cz>
2020-08-18 11:22:44 +0200

    Zeptání se na vstup a testování

--- 2020-08-07/Palindrom.py
+++ 2020-08-07/Palindrom.py
@@ -1,7 +1,15 @@
-slovo = input("Zadejte slovo: ")
-
 def je_palindrom(slovo):
     if slovo == slovo[::-1]: 
         return True
     else:
         return False
+
+assert je_palindrom("radar")
+assert je_palindrom("míč") == False
+
+slovo = input("Zadejte slovo: ")
+
+if je_palindrom(slovo): 
+    print("JE to palindrom")
+else:
+    print("NENÍ to palindrom")
