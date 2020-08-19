7b4639c509f71d9095ec05aad94374d78fcd395e
Dhoozy <maximpolak@email.cz>
2020-08-19 18:25:59 +0200

    Finální verze DÚ

--- 2020-08-07/Rozklad_na_prvočinitele.py
+++ 2020-08-07/Rozklad_na_prvočinitele.py
@@ -1,11 +1,32 @@
+""" 
+Máme číslo, zkusíme ho vydělit prvočíslem (nejnižším)
+Pokud to jde: --> uložíme si prvočíslo a vydělíme (nahradíme původní číslo číslem vyděleným), opakujeme, dokud do jde
+                pokud to už nejde, pokračujeme s "Pokud ne:"
+Pokud ne: --> změníme prvočíslo (prvocislo + 1)
+Řádek 3-5 opakujeme, dokud se nedostaneme na jedničku
+"""
+#28:2 -> 14:2 -> 7
+
 cislo = int(input("Zadejte číslo, které chcete rozložit na prvočinitele: "))
 
 def rozloz_na_prvocinitele(cislo):
+    prvocislo = 2
     seznam_prvocinitelu = []
-    for prvocinitel in range(1, cislo + 1):
-        if cislo % prvocinitel == 0:
-            seznam_prvocinitelu.append(prvocinitel)
-    return seznam_prvocinitelu
+
+    while True:
+        if cislo % prvocislo == 0:
+
+            seznam_prvocinitelu.append(prvocislo)
+            cislo = cislo / prvocislo
+
+            if cislo == 1:
+                break  
+        else:
+            prvocislo += 1
+    return seznam_prvocinitelu     
+
+assert rozloz_na_prvocinitele(28) == [2, 2, 7]
+assert rozloz_na_prvocinitele(54) == [2, 3, 3, 3]
 
 seznam = rozloz_na_prvocinitele(cislo)
 
