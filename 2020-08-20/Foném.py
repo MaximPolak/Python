3849ef9b0f50abc56b727fc36eb9c161c1524ff1
Maxim Polák <polakmaxi@gmail.com>
2020-08-20 11:52:35 +0200

    Vyjasnění kódu Přidání funkce zip v cyklu for

--- 2020-08-20/Foném.py
+++ 2020-08-20/Foném.py
@@ -8,17 +8,14 @@ def je_minimalni_par(slovo1, slovo2):
         delka = len(slovo1)
         pozice = 0
         zmeny = 0
-        for pozice in range(delka):
-            if slovo1[pozice] != slovo2[pozice]:
+        for (znak1, znak2) in zip(slovo1, slovo2):
+            if znak1 != znak2:
                 zmeny += 1
                 if zmeny == 2:
                     return False
         if zmeny == 1:
             return True
-        else:
-            return False
-    else:
-        return False
-                    
+    return False                    
 
 test_je_minimalni_par()
+
