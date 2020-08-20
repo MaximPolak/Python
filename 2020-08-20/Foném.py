f64e25caadfd96cc0956936a3aabb6e2d9772a61
Maxim Polák <polakmaxi@gmail.com>
2020-08-20 11:15:32 +0200

    Vytvoření testů

---
+++ 2020-08-20/Foném.py
@@ -0,0 +1,11 @@
+
+
+def test_je_minimalni_par():
+    assert je_minimalni_par('lety', 'ledy')
+    assert not je_minimalni_par('led', 'ledy')
+
+
+def je_minimalni_par(slovo1, slovo2):
+    pass
+
+test_je_minimalni_par()
