8206786e37d3471f0f10d2f51062783fb3d54940
Maxim Polák <polakmaxi@gmail.com>
2020-08-20 11:32:54 +0200

    Úspěšné testy Ošklivý kód

--- 2020-08-20/Foném.py
+++ 2020-08-20/Foném.py
@@ -1,11 +1,25 @@
-
-
 def test_je_minimalni_par():
     assert je_minimalni_par('lety', 'ledy')
     assert not je_minimalni_par('led', 'ledy')
 
 
 def je_minimalni_par(slovo1, slovo2):
-    pass
+    if len(slovo1) == len(slovo2):
+        delka = len(slovo1)
+        pozice = 0
+        zmeny = 0
+        while pozice < delka:
+            if slovo1[pozice] != slovo2[pozice]:
+                zmeny += 1
+                if zmeny == 2:
+                    return False
+            pozice += 1
+        if zmeny == 1:
+            return True
+        else:
+            return False
+    else:
+        return False
+                    
 
 test_je_minimalni_par()
