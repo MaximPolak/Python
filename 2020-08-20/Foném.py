0e58f5edfd5202f9fb48f4d8a4cc649d5d4ce254
Maxim Polák <polakmaxi@gmail.com>
2020-08-20 12:03:38 +0200

    Funkce na hledání lišících se znaků a testy

--- 2020-08-20/Foném.py
+++ 2020-08-20/Foném.py
@@ -2,20 +2,37 @@ def test_je_minimalni_par():
     assert je_minimalni_par('lety', 'ledy')
     assert not je_minimalni_par('led', 'ledy')
 
+def test_najdi_lisici_se_znaky():
+    assert najdi_lisici_se_znaky("let", "led") == ("t", "d")
+    assert najdi_lisici_se_znaky("let", "lety") == None
 
 def je_minimalni_par(slovo1, slovo2):
     if len(slovo1) == len(slovo2):
         delka = len(slovo1)
-        pozice = 0
         zmeny = 0
         for (znak1, znak2) in zip(slovo1, slovo2):
             if znak1 != znak2:
+                
                 zmeny += 1
                 if zmeny == 2:
                     return False
         if zmeny == 1:
             return True
-    return False                    
+    return False         
+
+def najdi_lisici_se_znaky(slovo1, slovo2):
+    if len(slovo1) == len(slovo2):
+        delka = len(slovo1)
+        zmeny = 0
+        for (znak1, znak2) in zip(slovo1, slovo2):
+            if znak1 != znak2:
+                zmeny += 1
+                lisici_se_znaky = (znak1, znak2)
+                if zmeny == 2:
+                    return None
+        if zmeny == 1:
+            return lisici_se_znaky
+    return None                               
 
 test_je_minimalni_par()
-
+test_najdi_lisici_se_znaky()
