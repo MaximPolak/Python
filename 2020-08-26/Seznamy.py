5efb0fadfca887c1fc03d6c6625bf73c8850c333
Maxim Polák <polakmaxi@gmail.com>
2020-08-26 11:43:36 +0200

    Upravení podmínky

--- 2020-08-26/Seznamy.py
+++ 2020-08-26/Seznamy.py
@@ -1,14 +1,12 @@
 seznam = []
 for i in range(1, 1001):
     seznam.append(i)
-print(seznam)
 
 suda_cisla = list(range(2, 1001, 2))
 
 seznam2 = []
 for i in range(1, 1001):
-    if i % 3 == 0:
-        seznam.append(i)
-    elif i % 5 == 0:
-        seznam.append(i)
+    if i % 3 == 0 or i % 5 == 0:
+        seznam2.append(i)
+print(seznam2)
     
