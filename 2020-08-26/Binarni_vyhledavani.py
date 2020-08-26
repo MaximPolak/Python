05c7b65c4cbd0b39662e517b768411b8d389a475
Maxim Polák <polakmaxi@gmail.com>
2020-08-26 08:56:28 +0200

    Hotový domácí úkol

---
+++ 2020-08-26/Binarni_vyhledavani.py
@@ -0,0 +1,20 @@
+def binarne_vyhledej(prvek, seznam):
+    zacatek = 0
+    konec = len(seznam) - 1
+
+    while zacatek <= konec:
+        pozice = (zacatek + konec) // 2
+        if seznam[pozice] == prvek:
+            return pozice
+        elif prvek < seznam[pozice]:
+            konec = pozice - 1
+        else:
+            zacatek = pozice + 1
+    return -1
+
+def test_hledani():
+    assert binarne_vyhledej(0, seznam) == 0
+    assert binarne_vyhledej(2, seznam) == -1
+
+seznam = [0,1,1,1,5]
+test_hledani()
