4052ffae29fc8fae1b09addb2ee6d82ab938b5ea
Maxim Polák <polakmaxi@gmail.com>
2020-08-26 10:39:03 +0200

    Detaily (closes #2)

--- 2020-08-26/Binarni_vyhledavani.py
+++ 2020-08-26/Binarni_vyhledavani.py
@@ -12,9 +12,9 @@ def binarne_vyhledej(prvek, seznam):
             zacatek = pozice + 1
     return -1
 
-def test_hledani():
+def test_binarne_vyhledej():
+    seznam = [0,1,1,1,5]
     assert binarne_vyhledej(0, seznam) == 0
     assert binarne_vyhledej(2, seznam) == -1
 
-seznam = [0,1,1,1,5]
-test_hledani()
+test_binarne_vyhledej()
