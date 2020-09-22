48dca8e1ece9a6dd95a3360dc56be9defd94526e
Maxim Polák <polakmaxi@gmail.com>
2020-09-22 22:17:27 +0200

    Domácí úkol - minimální páry Neumím si poradit s jedním krokem - popsaný v řetězci "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py"

---
+++ 2020-09-24/Minimální_páry.py
@@ -0,0 +1,26 @@
+with open("babicka.txt", encoding = "utf-8") as soubor:
+    cely_text = soubor.read()
+    slova = set(cely_text.split())
+
+def najdi_minimalni_pary(slova):
+    slovnik = {}
+
+    for slovo1 in slova:
+        for slovo2 in slova:
+            if len(slovo1) == len(slovo2):
+                rozdily = 0
+                for (znak1, znak2) in zip(slovo1, slovo2):
+                    if znak1 != znak2:
+                        rozdily += 1
+                        if znak1 < znak2:
+                            lisici_se_znaky = (znak1, znak2)
+                        else:
+                            lisici_se_znaky = (znak2, znak1)
+                if rozdily == 1:
+                    """
+                    if lisici_znaky not in slovnik:
+                        do knihovny přidat klíč(lisici_se_znaky)
+                    slovnik[lisici_se_znaky].append(slovo1, slovo2)
+                    """
+            
+print(najdi_minimalni_pary(slova))
