1c7dac66e68d76722695619ccf86d7b6496b4c81
Maxim Polák <polakmaxi@gmail.com>
2020-09-29 21:57:54 +0200

    Přidání možnosti řetězcového vstupu Dodělat další podmínky "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py"

--- 2020-10-01/Převádění_minut.py
+++ 2020-10-01/Převádění_minut.py
@@ -1,10 +1,15 @@
-def prevadeni_minut(minutovy_vstup):
-    cas = minutovy_vstup
-    dny = cas // (60 * 24)
-    cas %= 60 * 24
-    hodiny = cas // 60
-    cas %= 60
-    minuty = cas
-    print(f"{minutovy_vstup} min = {dny} d {hodiny} h {minuty} min")
+def prevadeni_minut(vstup):
+    rozdeleny_vstup = vstup.split()
+    for prvek in rozdeleny_vstup:
+        try:
+            cas = int(prvek)
+            dny = cas // (60 * 24)
+            cas %= 60 * 24
+            hodiny = cas // 60
+            cas %= 60
+            minuty = cas
+            print(f"{prvek} min = {dny} d {hodiny} h {minuty} min")
+        except ValueError:
+            print(f"CHYBA: {prvek} není nezáporné celé číslo")
 
-prevadeni_minut(828181818)
+prevadeni_minut("0 vznášedlo 62 5 46800")
