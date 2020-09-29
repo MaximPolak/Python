b40d9b69ca108b23bec9c12217ac512e958cfa0d
Maxim Polák <polakmaxi@gmail.com>
2020-09-29 22:12:52 +0200

    Finální verze DÚ "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py"

--- 2020-10-01/Převádění_minut.py
+++ 2020-10-01/Převádění_minut.py
@@ -1,15 +1,26 @@
 def prevadeni_minut(vstup):
-    rozdeleny_vstup = vstup.split()
-    for prvek in rozdeleny_vstup:
-        try:
-            cas = int(prvek)
+    if type(vstup) == str:
+        rozdeleny_vstup = vstup.split()
+        for prvek in rozdeleny_vstup:
+            try:
+                cas = int(prvek)
+                dny = cas // (60 * 24)
+                cas %= 60 * 24
+                hodiny = cas // 60
+                cas %= 60
+                minuty = cas
+                print(f"{prvek} min = {dny} d {hodiny} h {minuty} min")
+            except ValueError:
+                print(f"CHYBA: {prvek} není nezáporné celé číslo")
+    elif type(vstup) == int:
+            cas = vstup
             dny = cas // (60 * 24)
             cas %= 60 * 24
             hodiny = cas // 60
             cas %= 60
             minuty = cas
-            print(f"{prvek} min = {dny} d {hodiny} h {minuty} min")
-        except ValueError:
-            print(f"CHYBA: {prvek} není nezáporné celé číslo")
+            print(f"{vstup} min = {dny} d {hodiny} h {minuty} min")
+    else:
+        print(f"CHYBA: {vstup} není nezáporné celé číslo")
 
-prevadeni_minut("0 vznášedlo 62 5 46800")
+prevadeni_minut(input("Zadejte vstup:  "))
