777488e6fa9b78126de20f105a7f5d1ac06dacb2
Maxim Polák <polakmaxi@gmail.com>
2020-10-07 12:35:50 +0200

    Finální (snad dokonalá) verze úkolu "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py"

--- 2020-09-10/Bublinkové_řazení.py
+++ 2020-09-10/Bublinkové_řazení.py
@@ -1,33 +1,13 @@
 def bubble_sort(seznam):
-    """
-    [6, 5, 8, 1]
-    5, 6, 8, 1
-    5681
-    5618
-    2.kolo
-    5618
-    5168
-    5168
-    3.kolo
-    1568
-    """
-    puvodni_seznam = seznam
 
-    for i in range(len(seznam)):
+    for i in range(len(seznam) - 1):
+        zmeny = False
+
         for j in range(len(seznam) - 1):
-
-            if sorted(seznam) == seznam:
-                break
-
             if seznam[j] > seznam[j + 1]:
-                # seznam[j], seznam[j + 1] = seznam[j + 1], seznam[j]
-                j_vetsi_kopie = seznam[j + 1]
-                j_kopie = seznam[j]
-                seznam[j] = j_vetsi_kopie
-                seznam[j + 1] = j_kopie           
-
+                seznam[j], seznam[j + 1] = seznam[j + 1], seznam[j]
+                zmeny = True
+        if zmeny == False:
+            break
     return seznam
-
-seznam = [6, 5, 8, 1]
-print(bubble_sort(seznam))
 
