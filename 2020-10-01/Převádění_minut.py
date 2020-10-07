98f8e4a5adb1b67681e98d0020cce900d6d873f8
Maxim Polák <polakmaxi@gmail.com>
2020-10-07 15:17:11 +0200

    Finální (snad dokonalá) verze úkolu "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py"

--- 2020-10-01/Převádění_minut.py
+++ 2020-10-01/Převádění_minut.py
@@ -1,26 +1,21 @@
-def prevadeni_minut(vstup):
-    if type(vstup) == str:
+def pocitani_minut(vstup):
+    (dny, cas) = divmod(vstup, 60*24)
+    (hodiny, minuty) = divmod(cas, 60)
+    return (dny, hodiny, minuty)
+
+def prevadeni_vstupu(vstup):
+    if isinstance(vstup, str) == True:
         rozdeleny_vstup = vstup.split()
         for prvek in rozdeleny_vstup:
-            try:
-                cas = int(prvek)
-                dny = cas // (60 * 24)
-                cas %= 60 * 24
-                hodiny = cas // 60
-                cas %= 60
-                minuty = cas
+            if prvek.isdigit() == True:
+                (dny, hodiny, minuty) = pocitani_minut(int(prvek))
                 print(f"{prvek} min = {dny} d {hodiny} h {minuty} min")
-            except ValueError:
-                print(f"CHYBA: {prvek} není nezáporné celé číslo")
-    elif type(vstup) == int:
-            cas = vstup
-            dny = cas // (60 * 24)
-            cas %= 60 * 24
-            hodiny = cas // 60
-            cas %= 60
-            minuty = cas
-            print(f"{vstup} min = {dny} d {hodiny} h {minuty} min")
+            else:
+                print(f"CHYBA: '{prvek}' není nezáporné celé číslo")
+    elif isinstance(vstup, int) == True:
+        (dny, hodiny, minuty) = pocitani_minut(vstup)
+        print(f"{vstup} min = {dny} d {hodiny} h {minuty} min")
     else:
-        print(f"CHYBA: {vstup} není nezáporné celé číslo")
+        print(f"CHYBA: '{vstup}' není nezáporné celé číslo")
 
-prevadeni_minut(input("Zadejte vstup:  "))
+prevadeni_vstupu("0 vznášedlo 62 5 46800")
