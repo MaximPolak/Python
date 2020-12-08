03b0d54402456084ff8e1b777c9524752ee82f56
Maxim Polák <polakmaxi@gmail.com>
2020-12-08 23:27:33 +0100

    Malá úprava funkce pohni_se, aby fungovalo přepisování souřadnic "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py" "../2020-11-05/P\305\231evracen\303\255_slovn\303\255k\305\257.py" "../2020-10-22/Vylep\305\241en\303\251_bubilnkov\303\251_\305\231azen\303\255.py"

---
+++ 2020-12-03/Vektor.py
@@ -0,0 +1,38 @@
+class Vektor:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+    def __repr__(self):
+        return f"Vektor(x={self.x!r}, y={self.y!r})"
+    def __add__(self, druhy_vektor): 
+        return Vektor(self.x + druhy_vektor.x, self.y + druhy_vektor.y)
+    def __mul__(self, cislo):
+        return Vektor(self.x * cislo, self.y * cislo)
+    def __rmul__(self, cislo):
+        return self.__mul__(cislo) #self * cislo
+    def __div__(self, cislo):
+        return Vektor(self.x / cislo, self.y / cislo)
+    def __sub__(self, druhy_vektor):
+        return self + (-1 *druhy_vektor)
+
+sipka = Vektor(5, 0)
+sipka2 = Vektor(0, 5)
+
+SEZNAM_SMERU = (Vektor(0, 1), Vektor(1, 0), Vektor(0, -1), Vektor(-1, 0))
+class Objekt:
+    def __init__(self, souradnice, smer_i, rychlost):
+        self.souradnice, self.smer_i, self.rychlost = souradnice, smer_i, rychlost
+    def __repr__(self):
+        return f"Objekt(souradnice={self.souradnice!r}, smer_i={self.smer_i!r}, rychlost={self.rychlost!r})"
+    def pohni_se(self):
+        self.souradnice[0] += self.rychlost * SEZNAM_SMERU[self.smer_i].x
+        self.souradnice[1] += self.rychlost * SEZNAM_SMERU[self.smer_i].y
+    def otoc_se_doprava(self):
+        self.smer_i = (1 + self.smer_i) % 3
+    def otoc_se_doleva(self):
+        if self.smer_i == 0:
+            self.smer_i = 3
+        else:
+            self.smer_i = (-1 + self.smer_i )
+    def obrat_se(self):
+        for i in range(2):
+            self.otoc_se_doprava()
