a4d46025b196087b758ae1c328ba1e84d0ba334c
Maxim Polák <polakmaxi@gmail.com>
2020-12-10 01:25:53 +0100

    Změna souřadnic šneka na Vektor "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py" "../2020-11-05/P\305\231evracen\303\255_slovn\303\255k\305\257.py" "../2020-10-22/Vylep\305\241en\303\251_bubilnkov\303\251_\305\231azen\303\255.py"

--- 2020-12-03/sneci_prochazka.py
+++ 2020-12-03/sneci_prochazka.py
@@ -1,6 +1,6 @@
 from Vektor import Vektor, Objekt
 
-snek = Objekt([2, 3], 0, 1)
+snek = Objekt(souradnice=Vektor(2, 3), smer_i=0, rychlost=1)
 
 #vytvoření mapy
 mapa = []
@@ -12,7 +12,7 @@ for i in range(10):
 for i in range(4):
     for j in range(4):
         snek.pohni_se()
-        mapa[snek.souradnice[1] - 1][snek.souradnice[0] - 1] = ['*']
+        mapa[snek.souradnice.y - 1][snek.souradnice.x - 1] = ['*']
     snek.otoc_se_doprava()
 
 #tisknutí mapy
