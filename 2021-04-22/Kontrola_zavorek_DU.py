1aa7a39511db549e3b11413747b76bed9f22599a
Maxim Polák <polakmaxi@gmail.com>
2021-05-04 23:44:32 +0200

    Doplnění chyby v případě, že nejhlubší závorky nejsou kulaté "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py" "../2020-11-05/P\305\231evracen\303\255_slovn\303\255k\305\257.py" "../2020-10-22/Vylep\305\241en\303\251_bubilnkov\303\251_\305\231azen\303\255.py"

--- 2021-04-22/Kontrola_zavorek_DU.py
+++ 2021-04-22/Kontrola_zavorek_DU.py
@@ -3,30 +3,52 @@ import sys
 def kontrola_zavorek(vyraz):
     zasobnik = []
     pozice_chyby = None
+    nejhlubsi_leva = (0, "") # (úrověň_vnoření, typ_závorky)
+    pozice_nejhlubsich_zavorek = [0, 0]
     odpovidajici_zavorky = dict(zip(")]}", "([{"))
-    for pozice, i in enumerate(vyraz):
-        if i not in "()[]{}":
+    predchazejici_zavorky = dict(zip("([{", "[{("))
+    for pozice, znak in enumerate(vyraz):
+        if znak not in "()[]{}":
             continue
-        if i in "([{":
-            if len(zasobnik) == 0:
-                pozice_chyby = pozice
-            zasobnik.append(i)
-        elif len(zasobnik) == 0:
-            pozice_chyby = pozice
-            return (pozice_chyby, "CHYBA: Některé závorky nejsou otevřené.")
-        elif odpovidajici_zavorky[i] != zasobnik.pop():
-            pozice_chyby = pozice
-            return (pozice_chyby, "CHYBA: Některé závorky si neodpovídají.")
+        if znak in "([{":
+            if len(zasobnik) != 0 and predchazejici_zavorky[znak] != zasobnik[-1][0]:
+                pozice_chyby = (pozice,)
+                return (pozice_chyby, "CHYBA: Jdou po sobě špatné závorky") 
+            zasobnik.append((znak, pozice))
 
+            delka_zasobniku = len(zasobnik)
+            if nejhlubsi_leva[0] < delka_zasobniku:
+                nejhlubsi_leva = (delka_zasobniku, znak)
+                pozice_nejhlubsich_zavorek[0] = pozice
+                pamatovat_si_dalsi_zavrenou = True
+
+        elif len(zasobnik) == 0:
+            pozice_chyby = (pozice,)
+            return (pozice_chyby, "CHYBA: Některé závorky nejsou otevřené.")
+        else:
+            posledni_zavorka, index = zasobnik.pop()
+
+            if pamatovat_si_dalsi_zavrenou:
+                pozice_nejhlubsich_zavorek[1] = pozice
+                pamatovat_si_dalsi_zavrenou = False
+
+            if odpovidajici_zavorky[znak] != posledni_zavorka:
+                pozice_chyby = index, pozice
+                return (pozice_chyby, "CHYBA: Některé závorky si neodpovídají.")
+            
     if len(zasobnik) == 0:
+        if nejhlubsi_leva[1] not in ("", "("):
+            pozice_chyby = tuple(pozice_nejhlubsich_zavorek)
+            return (pozice_chyby, "CHYBA: Nejhlubší závorky nejsou kulaté")
         return (None, "OK")
     else:
+        pozice_chyby = tuple(i for _, i in zasobnik)
         return (pozice_chyby, "CHYBA: Některé závorky nejsou zavřené.")
 
-for i in sys.stdin:
-    hlasky = kontrola_zavorek(i)
-    if hlasky[0] != None:
-        seznam = [" " for i in range(hlasky[0])] + ["^"]
+for radek in sys.stdin:
+    pozice, hlaska = kontrola_zavorek(radek)
+    if hlaska != "OK":
+        seznam = [" " if i not in pozice else "^" for i in range(len(radek))]
         print("".join(seznam))
-    print(hlasky[1])
+    print(hlaska)
     
