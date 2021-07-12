8210ea7c3c8436b97e62d93e0daaf192b14adf81
Maxim Polák <polakmaxi@gmail.com>
2021-07-12 20:38:00 +0200

    Zjišťování výhry a vpisování do hrací plochy "../2020-08-28/Sl\303\251v\303\241n\303\255_Seznam\305\257.py" "../2020-11-05/P\305\231evracen\303\255_slovn\303\255k\305\257.py" "../2020-10-22/Vylep\305\241en\303\251_bubilnkov\303\251_\305\231azen\303\255.py"

---
+++ 2021-06-24/Piskvorky.py
@@ -0,0 +1,56 @@
+class ZvlastniError(Exception):
+    pass
+
+class Piskvorky:
+    def __init__(self, vyska, sirka, delka_vitezne_rady):
+        self.vyska = vyska
+        self.sirka = sirka
+        self.delka = delka_vitezne_rady
+        self.hraci_plocha = [[" " for _ in range(self.sirka)] for _ in range(self.vyska)]
+        self.smery = ((0, 1), (1, 0), (1, 1), (1, -1))
+    def indexy(self):
+        for radek in range(self.vyska):
+            for sloupec in range(self.sirka):
+                yield (radek, sloupec)
+        #return ((radek, sloupec) for radek in range(vyska) for sloupec in range(sirka))
+    def zjisti_index_moznych_vyher(self):
+        for radek, sloupec in self.indexy(): # radek, sloupec
+            for smer in self.smery:
+                ntice_indexu = tuple((radek + smer[0] * k, sloupec + smer[1] * k) for k in range(self.delka))
+                if ntice_indexu[-1] in self.indexy():
+                    yield ntice_indexu
+
+               #if all(ntice in self.indexy() for ntice in ntice_indexu):     !neúsporné
+                   #yield ntice_indexu
+    def zjisti_vyhru(self):
+        for indexy_vyhry in self.zjisti_index_moznych_vyher():
+            ntice = tuple(self.hraci_plocha[radek][sloupec] for radek, sloupec in indexy_vyhry)
+            if ntice[0] != " " and ntice.count(ntice[0]) == len(ntice):
+                return ntice[0]
+        return None
+
+    def vepis_do_pole(self, index_pole, znak):
+        if self.hraci_plocha[index_pole[0]][index_pole[1]] == " ":
+            self.hraci_plocha[index_pole[0]][index_pole[1]] = znak
+        else:
+            raise ZvlastniError   
+
+"""
+1 2 3           1: 2,3 ; 4,7 ; 5,9
+4 5 6           2: 5,8
+7 8 9       d   3: 6,9 ; 5,7
+if sloupec + delka < sirka
+if radek + delka < vyska
+if 0 <= sloupec - delka < sirka
+if oboje
+
+i
+
+r, s; r, s + 1; r, s + 2 --> (0, 1)
+r, s; r + 1, s; r + 2, s --> (1, 0)
+r, s; r + 1, s + 1; r + 2, s + 2 --> (1, 1)
+r, s; r + 1, s - 1; r + 2, s - 2 --> (1, -1)
+
+(r, s), (r + a, s + b), (r + 2a, s + 2b)
+(r + ka, s + kb)
+"""
